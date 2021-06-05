0e9a541e791ecbf79e92145c5a74789dbba4248b
EmbraceUU <1014435409@qq.com>
2021-06-05 13:14:56 +0800

    build: 调整 caesar-boot 项目结构

--- caesar-graphql/src/main/java/com/demo/graphql/GraphqlApplication.java
+++ caesar-graphql/src/main/java/com/demo/graphql/GraphqlApplication.java
@@ -2,12 +2,12 @@ package com.demo.graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching // 开启缓存
 public class GraphqlApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GraphqlApplication.class, args);
     }
-
 }
