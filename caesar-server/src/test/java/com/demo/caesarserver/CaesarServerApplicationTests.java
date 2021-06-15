a4b9eec04a5da6152c3070a31ba01cfa617587e2
EmbraceUU <1014435409@qq.com>
2021-06-15 13:56:24 +0800

    study: spring-boot schedule

---
+++ caesar-server/src/test/java/com/demo/caesarserver/CaesarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.caesarserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaesarServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
